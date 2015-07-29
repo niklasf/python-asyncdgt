8e5bc3b8dbaec49cda623cf3689be47d8cf07d70
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-07-29 18:29:55 +0200

    Incremental update support

--- asyncdgt.py
+++ asyncdgt.py
@@ -7,6 +7,9 @@ DGT_SEND_BRD = 0x42
 
 DGT_BOARD_DUMP = 0x86
 
+DGT_SEND_UPDATE_NICE = 0x4b
+
+DGT_MSG_FIELD_UPDATE = 0x80 | 0x0e
 
 PIECE_TO_CHAR = {
     0x01: "P",
@@ -24,11 +27,46 @@ PIECE_TO_CHAR = {
     0x00: "."
 }
 
+class Board(object):
+
+    def __init__(self, connection):
+        self.connection = connection
+        self.state = bytearray(0x00 for _ in range(64))
+
+    def process_message(self, message_id, message):
+        if message_id == DGT_BOARD_DUMP:
+            self.state = bytearray(message)
+        elif message_id == DGT_MSG_FIELD_UPDATE:
+            self.state[message[0]] = message[1]
+
+        print(self.fen())
+
+    def fen(self):
+        fen = []
+        empty = 0
+
+        for index, c in enumerate(self.state):
+            if not c:
+                empty += 1
+
+            if empty > 0 and (c or (index + 1) % 8 == 0):
+                fen.append(str(empty))
+                empty = 0
+
+            if c:
+                fen.append(PIECE_TO_CHAR[c])
+
+            if (index + 1) % 8 == 0 and index < 63:
+                fen.append("/")
+
+        return "".join(fen)
+
 
 class Connection(object):
 
     def __init__(self, port, loop):
         self.loop = loop
+        self.board = Board(self)
 
         self.serial = serial.Serial(port,
             stopbits=serial.STOPBITS_ONE,
@@ -37,28 +75,8 @@ class Connection(object):
 
         loop.add_reader(self.serial, self.can_read)
 
+        self.serial.write(bytearray([DGT_SEND_UPDATE_NICE]))
         self.serial.write(bytearray([DGT_SEND_BRD]))
-
-    def process_board_dump(self, message):
-        fen = []
-        empty = 0
-
-        for index, c in enumerate(message):
-            if not c:
-                empty += 1
-
-            if empty > 0:
-                if c or (index + 1) % 8 == 0:
-                    fen.append(str(empty))
-                    empty = 0
-
-            if c:
-                fen.append(PIECE_TO_CHAR[c])
-
-            if (index + 1) % 8 == 0 and index < 63:
-                fen.append("/")
-
-        print("".join(fen))
 
     def can_read(self):
         header = self.serial.read(3)
@@ -66,10 +84,14 @@ class Connection(object):
         message_length = (header[1] << 7) + header[2] - 3
         message = self.serial.read(message_length)
 
-        print(message_id, message)
+        self.board.process_message(message_id, message)
 
         if message_id == DGT_BOARD_DUMP:
             self.process_board_dump(message)
+        elif message_id == DGT_MSG_FIELD_UPDATE:
+            print(message)
+        else:
+            print(hex(message_id), message)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
