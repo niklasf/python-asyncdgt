61ea2eafa0efb8f3e58130328ee615c8cc1a3771
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-09-13 17:46:06 +0200

    Clean up main script

--- asyncdgt/__init__.py
+++ asyncdgt/__init__.py
@@ -1,4 +1,26 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of the python-asyncdgt library.
+# Copyright (C) Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "Niklas Fiekas"
+
+__email__ = "niklas.fiekas@tu-clausthal.de"
+
+__version__ = "0.0.1"
+
 
 import asyncio
 import serial
@@ -283,7 +305,7 @@ class Connection(pyee.EventEmitter):
         pass
 
     def __enter__(self):
-        if self.connect()
+        if self.connect():
             return self
         else:
             raise IOError("dgt board not connected")
--- asyncdgt/__main__.py
+++ asyncdgt/__main__.py
@@ -1,44 +1,92 @@
+# This file is part of the python-asyncdgt library.
+# Copyright (C) 2015 Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+The asyncdgt library.
+Copyright (C) 2015 Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
+
+Usage:
+  python -m asyncdgt <dgt-port>
+
+<dgt-port>
+  The serial port with the DGT board.
+"""
+
 import asyncio
 import asyncdgt
 import logging
 import sys
+import serial
+import serial.tools.list_ports
 
-port_globs = sys.argv[1:]
-if not port_globs:
-    print("Usage: python -m asyncdgt <dgt-port>")
-    print("  for example /dev/ttyACM0")
-    sys.exit(1)
 
 logging.basicConfig(level=logging.DEBUG)
 
-loop = asyncio.get_event_loop()
 
-dgt = asyncdgt.auto_connect(sys.argv[1:], loop)
+def usage():
+    # Print usage information.
+    print(__doc__.strip())
 
-@dgt.on("connected")
-def on_connected(port):
-    print("Connected to {0}!".format(port))
-    #print("Version:", loop.run_until_complete(dgt.get_version()))
-    #print("Serial:", loop.run_until_complete(dgt.get_serialnr()))
-    #print("Long serial:", loop.run_until_complete(dgt.get_long_serialnr()))
-    #print("Board:", loop.run_until_complete(dgt.get_board()))
-    #print("Battery status:", loop.run_until_complete(dgt.get_battery_status()))
+    # List the available ports.
+    print("  Probably one of:")
+    for dev, name, info in serial.tools.list_ports.comports():
+        print("  * {0} ({1})".format(dev, info))
 
-@dgt.on("disconnected")
-def on_disconnected():
-    print("Disconnected!")
+    return 1
 
-@dgt.on("board")
-def on_board(board):
-    print("FEN: {0}".format(board.board_fen()))
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
-finally:
-    dgt.close()
+def main(port_globs):
+    loop = asyncio.get_event_loop()
 
-    pending = asyncio.Task.all_tasks(loop)
-    loop.run_until_complete(asyncio.gather(*pending))
-    loop.close()
+    dgt = asyncdgt.auto_connect(port_globs, loop)
+
+    @dgt.on("connected")
+    def on_connected(port):
+        print("Board connected to {0}!".format(port))
+
+    @dgt.on("disconnected")
+    def on_disconnected():
+        print("Board disconnected!")
+
+    @dgt.on("board")
+    def on_board(board):
+        print("FEN: {0}".format(board.board_fen()))
+
+    # Get some information.
+    print("Version:", loop.run_until_complete((dgt.get_version())))
+    print("Serial:", loop.run_until_complete(dgt.get_serialnr()))
+    print("Long serial:", loop.run_until_complete(dgt.get_long_serialnr()))
+    print("Board:", loop.run_until_complete(dgt.get_board()))
+    print("Battery status:", loop.run_until_complete(dgt.get_battery_status()))
+
+    # Run the event loop.
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        dgt.close()
+
+        pending = asyncio.Task.all_tasks(loop)
+        loop.run_until_complete(asyncio.gather(*pending))
+        loop.close()
+
+if __name__ == "__main__":
+    port_globs = sys.argv[1:]
+    if not port_globs:
+        sys.exit(usage())
+    else:
+        sys.exit(main(port_globs))
